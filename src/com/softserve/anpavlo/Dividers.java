bb2764ba178d2ebc05c87b129c74c514490c06f8
anpavlo <anpavlo@ukr.net>
2016-02-18 20:21:43 +0200

    update dividers

--- src/com/softserve/anpavlo/Dividers.java
+++ src/com/softserve/anpavlo/Dividers.java
@@ -4,8 +4,7 @@ public class Dividers {
 	
 	public static void getDividers(Integer n){
 		int i = 1;
-		System.out.println("Dividers of "+n);
-		
+		System.out.println("Dividers of "+n);		
 		while (i<=n/2){// ділимо на 2 оскільки найбільший дільник крім самого числа є його половина
 			if (n%i==0){
 				System.out.println(i);
