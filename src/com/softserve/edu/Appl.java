7481a2154d1b9d53508879568091b1d8bcdfc65f
softservedata <wapgprs@list.ru>
2016-02-19 10:30:23 +0200

    Add Simple JUnit Tests

--- src/com/softserve/edu/Appl.java
+++ src/com/softserve/edu/Appl.java
@@ -3,6 +3,16 @@ package com.softserve.edu;
 public class Appl {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		//
+		Calc calc = new Calc();
+		System.out.println("1 + 2 = " + calc.add(1, 2));
+		System.out.println("4 / 2 = " + calc.div(4, 2));
+		System.out.println("Info: " + calc.hello("Ivan"));
+		//
+		double d = 1.0 / 0.0;
+		System.out.println("double 1 / 0 = " + d);
+		int i = 1 / 0;
+		//System.out.println("int 1 / 0 = " + i);
 	}
-	
+
 }
---
+++ src/com/softserve/edu/Calc.java
@@ -0,0 +1,17 @@
+package com.softserve.edu;
+
+public class Calc {
+
+	public double add(double arg0, double arg1) {
+		return arg0 + arg1;
+	}
+
+	public double div(double arg0, double arg1) {
+		return arg0 / arg1;
+	}
+
+	public String hello(String name) {
+		return "Hello " + name;
+	}
+
+}
